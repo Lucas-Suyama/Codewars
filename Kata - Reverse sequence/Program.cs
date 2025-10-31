961f98f321c22871f28cb0fdd4c6fb3005794586
Lucas Suyama <lucasuyama@gmail.com>
2025-10-31 16:00:12 -0300

    feat: add 'Reverse sequence' solution with test cases

---
+++ Kata - Reverse sequence/Program.cs
@@ -0,0 +1,20 @@
+﻿public static class Kata
+{
+  public static int[] ReverseSeq(int n)
+  {
+    int[] vet = new int[n];
+    
+    for(int i=0; n > 0; i++ ){
+      vet[i] = n;
+      n--;
+    }
+      return vet;
+  }
+  public static void Main(string[] args)
+  {
+    int[] vet = ReverseSeq(5);
+    foreach(int i in vet){
+      Console.WriteLine(i);
+    }
+  }
+}
