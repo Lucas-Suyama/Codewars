346ce17e2e90197baec95e926ef599cc9402a26b
Lucas Suyama <lucasuyama@gmail.com>
2025-10-31 15:42:11 -0300

    feat: add String repeat Kata

---
+++ String repeat/Program.cs
@@ -0,0 +1,17 @@
+﻿public static class Kata
+{
+  public static string RepeatStr(int n, string s)
+  {
+    string result = "";
+
+    for (int i = 0; i < n; i++)
+      result += s;
+
+    return result;
+  }
+  public static void Main(string[] args)
+  {
+    Console.WriteLine(RepeatStr(3, "a"));
+    Console.WriteLine(RepeatStr(5, "hello"));
+  }
+}
